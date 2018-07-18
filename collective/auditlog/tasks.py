b9433b551930b00d0989e13fb6012422f44c8755
Joshua LaPlace <joshua.laplace@enfoldsystems.com>
2018-07-18 08:58:22 -0500

    Add missing tasks.py

---
+++ collective/auditlog/tasks.py
@@ -0,0 +1,12 @@
+from celery.utils.log import get_task_logger
+from collective.celery import task
+
+
+logger = get_task_logger(__name__)
+
+
+@task(name="audit")
+def queue_job(obj, *args, **kwargs):
+    from collective.auditlog.async import runJob
+    logger.warn('Logging action on {}'.format(obj.absolute_url_path()))
+    runJob(obj, *args, **kwargs)
